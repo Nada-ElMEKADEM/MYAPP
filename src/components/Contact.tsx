04adafb690397bd8cdb2f2dc30e532dbba61ceb2
Nada-ElMEKADEM <elmekademnada@gmail.com>
2023-11-06 20:53:00 +0100

    first commit

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,25 +1,83 @@
-export const Contact=() =>{
-    return(  
-        <div>
-       <h2 className="text-[#9b59b6] font-extrabold text-2xl">Contact</h2>
+import React, { useState } from 'react';
 
-       <div className="flex flex-wrap justify-between" >
-                <label className="block font-serif" >Email</label>
-                <input  className="border p-2 h-12 w-full rounded-lg "type="text" id="email" name="email" placeholder="Saisir votre email" required/>
-               
-     
-   
-   
-         <label className="block font-serif">Adresse</label>
-         <input className="border p-2 h-12 w-full rounded-lg" type="text"id="adresse" name="adresse"placeholder="Saisir votre adresse" required/>
-         
-    
-         <span className="block font-serif">Télèphone</span>
-         <input className="border p-2 h-12 w-full rounded-lg"  type="text" id="telephone" name="telephone"placeholder="Saisir votre numèro de telephone" required/>
-     </div>
-    
+export const Contact = () => {
+  const [email, setEmail] = useState('');
+  const [adresse, setAdresse] = useState('');
+  const [emailError, setEmailError] = useState<string | null>(null);
+  const [specialCharsError, setSpecialCharsError] = useState<string | null>(null);
 
- </div>
+  const validateEmail = (value: string): boolean => {
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    return emailRegex.test(value);
+  };
 
-    );
-}
+  const containsSpecialChars = (value: string): boolean => {
+    const specialCharsRegex = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\]/;
+    return specialCharsRegex.test(value);
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    setEmail(newValue);
+
+    if (validateEmail(newValue)) {
+      setEmailError(null);
+    } else {
+      setEmailError('Email invalide');
+    }};
+    const handleAdresseChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newValue = e.target.value;
+        setAdresse(newValue);
+
+    if (containsSpecialChars(newValue)) {
+      setSpecialCharsError('L\'adresse contient des caractères spéciaux');
+    } else {
+      setSpecialCharsError(null);
+    }
+  };
+
+
+  return (
+    <div>
+      <h2 className="text-[#9b59b6] font-extrabold text-2xl">Contact</h2>
+
+      <div className="flex flex-wrap justify-between">
+        <label className="block font-serif">Email</label>
+        <input
+          className="border p-2 h-12 w-full rounded-lg"
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Saisir votre email"
+          required
+          value={email}
+          onChange={handleEmailChange}
+        />
+        {emailError && <p className="text-red-500">{emailError}</p>}
+      </div>
+
+      <label className="block font-serif">Adresse</label>
+      <input
+        className="border p-2 h-12 w-full rounded-lg"
+        type="text"
+        id="adresse"
+        name="adresse"
+        placeholder="Saisir votre adresse"
+        required
+        value={adresse}
+        onChange={handleAdresseChange}
+      />
+      {specialCharsError && <p className="text-red-500">{specialCharsError}</p>}
+
+      <span className="block font-serif">Téléphone</span>
+      <input
+        className="border p-2 h-12 w-full rounded-lg"
+        type="tel"
+        id="telephone"
+        name="telephone"
+        placeholder="Saisir votre numéro de téléphone"
+        required
+      />
+    </div>
+  );
+};
--- src/components/Infoperso.tsx
+++ src/components/Infoperso.tsx
@@ -1,36 +1,102 @@
-export const Infoperso = () =>{
-    return (
-       
-       <div >
-         <h2 className="text-[#9b59b6] font-extrabold text-2xl">Informations personnelles</h2>
+import React, { useState } from 'react';
+export const Infoperso = () => {
+  const [nom, setNom] = useState('');
+  const [prenom, setPrenom] = useState('');
+  const [date, setDate] = useState('');
+  const [age, setAge] = useState(0);
+  const [sexe, setSexe] = useState('');
+  const [nomError, setNomError] = useState<string | null>(null);
+  const [prenomError, setPrenomError] = useState<string | null>(null);
+  const [ageError, setAgeError] = useState<string | null>(null);
 
-                <div className="flex flex-wrap justify-between" >
-                <label className="block font-serif" >Nom</label>
-                <input  className="border p-2 h-12 w-full rounded-lg "type="text" id="nom" name="nom" placeholder="Saisir votre nom" required/>
-               
-        
-    
-                <label className="block font-serif" >Prénom</label>
-                <input  className="border p-2 h-12 w-full rounded-lg " type="text" id="prenom" name="prenom" placeholder="Saisir votre prénom" required />
-            
-    
+  const handleNomChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    setNom(newValue);
 
-            
-                <label className="block font-serif">Date de naissance</label>
-                <input className="border p-2 h-12 w-full rounded-lg "type="date" id="date" name="date" required />
-        
-                <label className="block font-serif">Age</label>
-                < input className="border p-2 h-12 w-full rounded-lg "  id="Age" name="age" type="number" placeholder="xx" required />
-                <label className="block font-serif">Sexe</label>
-                <input type="radio" id="check-male" name="gender" />
-                        <label>Homme</label>
-    
-                        <input type="radio" id="check-female" name="gender"/>
-                        <label >Femme</label>
-                
-                </div>
-        </div>
+    if (newValue.toLowerCase() === newValue) {
+      setNomError('Le nom doit être en majuscules');
+    } else {
+      setNomError(null);
+    }
+  };
 
-    );
-}
-    
+  const handlePrenomChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    setPrenom(newValue);
+
+    if (newValue.length > 0 && newValue[0] !== newValue[0].toUpperCase()) {
+      setPrenomError('La première lettre du prénom doit être en majuscule');
+    } else {
+      setPrenomError(null);
+    }
+  };
+
+  const handleAgeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = parseInt(e.target.value, 10);
+    setAge(newValue);
+
+    if (newValue <= 18) {
+      setAgeError("L'âge doit être supérieur à 18");
+    } else {
+      setAgeError(null);
+    }
+  };
+
+  return (
+    <div>
+      <h2 className="text-[#9b59b6] font-extrabold text-2xl">Informations personnelles</h2>
+
+      <div className="flex flex-wrap justify-between">
+        <label className="block font-serif">Nom</label>
+        <input
+          className="border p-2 h-12 w-full rounded-lg"
+          type="text"
+          id="nom"
+          name="nom"
+          placeholder="Saisir votre nom"
+          required
+          value={nom}
+          onChange={handleNomChange}
+        />
+        {nomError && <p className="text-red-500">{nomError}</p>}
+
+        <label className="block font-serif">Prénom</label>
+        <input
+          className="border p-2 h-12 w-full rounded-lg"
+          type="text"
+          id="prenom"
+          name="prenom"
+          placeholder="Saisir votre prénom"
+          required
+          value={prenom}
+          onChange={handlePrenomChange}
+        />
+        {prenomError && <p className="text-red-500">{prenomError}</p>}
+
+        <label className="block font-serif">Date de naissance</label>
+        <input
+          className="border p-2 h-12 w-full rounded-lg"
+          type="date"
+          id="date"
+          name="date"
+          required
+        />
+
+        <label className="block font-serif">Âge</label>
+        <input
+          className="border p-2 h-12 w-full rounded-lg"
+          id="Age" name="age" type="number" placeholder="Âge" required
+          value={age}
+          onChange={handleAgeChange}
+        />
+        {ageError && <p className="text-red-500">{ageError}</p>}
+
+        <label className="block font-serif">Sexe</label>
+        <input type="radio" id="check-male" name="gender" />
+        <label>Homme</label>
+        <input type="radio" id="check-female" name="gender" />
+        <label>Femme</label>
+      </div>
+    </div>
+  );
+};
